c9c2e289f21e4b3cb51228e6fa1a6d6d46b71be3
Saudub <motivated.saurabh@gmail.com>
2025-03-26 10:58:47 +0530

    Commit from GitStuff

--- src/test/java/completeFlow/MyFlow.java
+++ src/test/java/completeFlow/MyFlow.java
@@ -46,6 +46,8 @@ public class MyFlow {
 		
 		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 		FileUtils.copyFile(src, new File(System.getProperty("user.dir")+"//reports//"+"Screenshot"+".png"));
+		driver.getWindowHandles();
+		driver.getCurrentUrl();
 		
 
 		
